a2bb67287566746388591181a29d94c7816586fd
OMI786I <sohrawardy1998@gmail.com>
2024-09-19 15:46:06 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./root/Root";
+import Home from "./Page/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      // {
+      //   path: "/Games",
+      //   element: <PagesToRead></PagesToRead>,
+      // },
+      // {
+      //   path: "/Scores",
+      //   element: <ListedBooks></ListedBooks>,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
