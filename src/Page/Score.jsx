d23a03d5fbab9cfca852f8f7a676019134defac4
OMI786I <sohrawardy1998@gmail.com>
2024-09-20 15:09:34 +0600

    getting started with score section

--- src/Page/Score.jsx
+++ src/Page/Score.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Score = () => {
-  return <div>This is scores</div>;
+  const [asc, setAsc] = useState(true);
+
+  return (
+    <div>
+      <h1 className="text-center font-bold underline text-3xl">Score</h1>
+      <div className="flex justify-center">
+        {" "}
+        <div className="flex flex-col w-[30%] ">
+          <h1 className="font-bold">sort scores</h1>
+          <button className="btn btn-warning" onClick={() => setAsc(!asc)}>
+            {asc ? "Low to High" : "High to Low"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Score;
