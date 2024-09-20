a804b8f54088f4e525b65d6381bf4bf88762ef83
OMI786I <sohrawardy1998@gmail.com>
2024-09-20 16:11:21 +0600

    basic sorting of scores

--- src/Page/Score.jsx
+++ src/Page/Score.jsx
@@ -1,7 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 
 const Score = () => {
   const [asc, setAsc] = useState(true);
+  const { isPending, error, data, refetch } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch(`http://localhost:5000/score?sort=${asc ? "asc" : "desc"}`).then(
+        (res) => res.json()
+      ),
+  });
+
+  console.log(data);
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <div>
@@ -10,9 +23,40 @@ const Score = () => {
         {" "}
         <div className="flex flex-col w-[30%] ">
           <h1 className="font-bold">sort scores</h1>
-          <button className="btn btn-warning" onClick={() => setAsc(!asc)}>
+          <button
+            className="btn btn-warning"
+            onClick={() => {
+              setAsc(!asc), refetch();
+            }}
+          >
             {asc ? "Low to High" : "High to Low"}
           </button>
+        </div>
+      </div>
+
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+
+              {data.map((res, index) => (
+                <tr key={res._id}>
+                  <th>{index + 1}</th>
+                  <td>{res.name}</td>
+                  <td>{res.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import Games from "./Page/Games";
 import Score from "./Page/Score";
 import Game from "./Page/Game";
 import TicTacToe from "./Game/TicTacTOe/TicTacToe";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,6 +42,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
