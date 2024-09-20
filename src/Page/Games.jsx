f80c2c85b3672a3a06e849ede9a762775208178c
OMI786I <sohrawardy1998@gmail.com>
2024-09-20 10:44:23 +0600

    getting started with tic tac toe

--- src/Page/Games.jsx
+++ src/Page/Games.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Game from "./Game";
+import { Link } from "react-router-dom";
 
 const Games = () => {
   const [data, setData] = useState([]);
@@ -12,7 +13,25 @@ const Games = () => {
 
   return (
     <div>
-      <Game></Game>
+      <h1 className="font-bold text-3xl text-center">Games List</h1>
+      <div className="md:flex gap-5">
+        {data.map((res) => (
+          <div key={res.title} className="card bg-base-100 w-80 shadow-xl">
+            <figure>
+              <img src={res.image} className="w-[50%]" alt="image" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{res.title}</h2>
+              <Link to={res.link}>
+                {" "}
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Play</button>
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
